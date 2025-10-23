903b9954fa8f54b390410f8552477420e9952e84
shanghai636848 <598499044@qq.com>
2025-10-24 05:16:09 +0800

    2.0.0版本可用

--- Zhg.FlowForge.App/Zhg.FlowForge.App.Web/Program.cs
+++ Zhg.FlowForge.App/Zhg.FlowForge.App.Web/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Localization;
-using System.Globalization;
-using Zhg.FlowForge.App.Shared.Interfaces;
-using Zhg.FlowForge.App.Shared.Models;
 using Zhg.FlowForge.App.Shared.Services;
 using Zhg.FlowForge.App.Web.Components;
 
@@ -14,34 +10,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddScoped<ScrollService>();
 
-// 配置应用设置
-builder.Services.Configure<AppSettings>(options =>
-{
-    options.DefaultCulture = "zh-CN";
-    options.ResourceSource = "JSON";
-    options.JsonResourcesPath = "localization";
-    options.UseBuiltInResources = true;
-});
-
-builder.Services.AddScoped<IFileSystemService, FileSystemService>();
-builder.Services.AddScoped<IJsonResourceService, JsonResourceService>();
-builder.Services.AddScoped<BlazorPreferenceService>();
-builder.Services.AddScoped<IPreferenceService>(provider =>
-    provider.GetRequiredService<BlazorPreferenceService>());
-builder.Services.AddScoped<ILocalizationService, LocalizationService>();
-
-
 var app = builder.Build();
-
-// 初始化本地化服务
-await using (var scope = app.Services.CreateAsyncScope())
-{
-    var localizationService = scope.ServiceProvider.GetRequiredService<ILocalizationService>();
-    await localizationService.InitializeAsync();
-}
-
-// 使用本地化中间件
-app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
